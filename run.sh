eb5aa65a04b403dc34954cf23b67043080746786
ahmetsaz <35495543+ahmetsaz@users.noreply.github.com>
2022-09-22 19:05:42 +0300

    [skip ci]

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -H 'Content-Type: application/json' -d '{payload:{commitId:123}}' http://rancher-ui.northcentralus.cloudapp.azure.com:8080/generic-webhook-trigger/invoke?token=testPipeline
+curl -X POST -H 'Content-Type: application/json' -d '{commitId:$1}' http://rancher-ui.northcentralus.cloudapp.azure.com:8080/generic-webhook-trigger/invoke?token=testPipeline
